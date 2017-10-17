093878e3016eceddd988a89d82b39e1da34ba20c
Demian Arenas <arenasdemian99@gmail.com>
2017-10-17 16:48:23 -0500

    Webpack integration and move index file

---
+++ src/index.js
@@ -0,0 +1,22 @@
+'use strict'
+import express from 'express'
+import bodyParser from 'body-parser'
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express'
+import { createServer } from 'http'
+import { executableSchema } from './data/schema'
+
+const HOST_PORT = 3000
+const GRAPHQL_PATH = '/graphql';
+
+const app = express()
+
+app.use('/graphql', bodyParser.json(), graphqlExpress(req => ({
+  schema: executableSchema,
+})));
+
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: GRAPHQL_PATH
+}));
+
+createServer(app).listen(HOST_PORT)
+console.log('Server runing')
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path')
+const fs = require('fs')
+const webpack = require('webpack')
+var pathout = './build/'
+var pathentry = './src/index.js'
+const nodeModules = fs
+  .readdirSync('node_modules')
+  .filter(x => ['.bin'].indexOf(x) === -1)
+  .reduce(
+  (modules, module) => Object.assign(modules, { [module]: `commonjs ${module}` }),
+  {}
+  )
+
+const config = {
+  entry: pathentry,
+  output: {
+    filename: 'index.js',
+    path: path.join(process.cwd(), pathout)
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/,
+        query: {
+          env: {
+            production: {
+              presets: ['es2016', 'es2017']
+            },
+            development: {
+              presets: ['latest-minimal']
+            }
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  target: 'node',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(true)
+  ],
+  externals: nodeModules
+}
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      mangle: {
+        except: ['$super', '$', 'exports', 'require']
+      }
+    })
+  )
+}
+
+module.exports = config
